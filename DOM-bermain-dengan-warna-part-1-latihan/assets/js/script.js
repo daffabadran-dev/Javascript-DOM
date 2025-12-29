41475109011c84a0918b87176ea445abc63c3ad2
Daffa Badran <daffabadran.dev@gmail.com>
2025-12-29 12:11:16 +0700

    memperbaiki kode scroll perubahan warna

--- DOM-bermain-dengan-warna-part-1-latihan/assets/js/script.js
+++ DOM-bermain-dengan-warna-part-1-latihan/assets/js/script.js
@@ -55,22 +55,34 @@ const sMerah = document.querySelector("input[name=sMerah]");
 const sHijau = document.querySelector("input[name=sHijau]");
 const sBiru = document.querySelector("input[name=sBiru]");
 
-//
-sMerah.addEventListener("input", function () {
+// //
+// sMerah.addEventListener("input", function () {
+//   const r = sMerah.value;
+//   const g = sHijau.value;
+//   const b = sBiru.value;
+//   document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+// });
+// sHijau.addEventListener("input", function () {
+//   const r = sMerah.value;
+//   const g = sHijau.value;
+//   const b = sBiru.value;
+//   document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+// });
+// sBiru.addEventListener("input", function () {
+//   const r = sMerah.value;
+//   const g = sHijau.value;
+//   const b = sBiru.value;
+//   document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+// });
+
+function updateBackgroundColor() {
   const r = sMerah.value;
   const g = sHijau.value;
   const b = sBiru.value;
   document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-});
-sHijau.addEventListener("input", function () {
-  const r = sMerah.value;
-  const g = sHijau.value;
-  const b = sBiru.value;
-  document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-});
-sBiru.addEventListener("input", function () {
-  const r = sMerah.value;
-  const g = sHijau.value;
-  const b = sBiru.value;
-  document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-});
+}
+
+sBiru.addEventListener("input", updateBackgroundColor);
+sMerah.addEventListener("input", updateBackgroundColor);
+sHijau.addEventListener("input", updateBackgroundColor);
+// updateBackgroundColor();
