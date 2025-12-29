e45133551b8ac6cca85510f2f4133dd88af65632
Daffa Badran <daffabadran.dev@gmail.com>
2025-12-29 10:39:15 +0700

    membuat tombol kedua, blm memiliki event

--- DOM-bermain-dengan-warna-part-1-latihan/assets/js/script.js
+++ DOM-bermain-dengan-warna-part-1-latihan/assets/js/script.js
@@ -4,6 +4,7 @@
 const tombol = document.createElement("button");
 const teksTombol = document.createTextNode("klik untuk mengubah warna");
 tombol.textContent = "klik saya, ";
+//
 tombol.setAttribute("type", "button");
 tombol.id = "tombolWarna";
 //
@@ -19,9 +20,21 @@ tombol.style.cursor = "pointer";
 //
 // event mengubah warna background
 tombol.addEventListener("click", function () {
-  document.body.style.backgroundColor = "lightblue";
+  // document.body.style.backgroundColor = "lightblue";
+  document.body.classList.toggle("biru-muda");
 });
-tombol.addEventListener("dblclick", function () {
-  document.body.style.backgroundColor = "";
-});
+// tombol.addEventListener("dblclick", function () {
+//   document.body.classList.toggle("abu-abu");
+// });
 //
+// membuat tombol kedua
+const tombolKedua = document.createElement("button");
+document.body.append(tombolKedua);
+tombolKedua.textContent = "klik saya, untuk warna random";
+//
+tombolKedua.style.padding = "10px";
+tombolKedua.style.color = "white";
+tombolKedua.style.backgroundColor = "salmon";
+tombolKedua.style.border = "none";
+tombolKedua.style.borderRadius = "10px";
+tombolKedua.style.cursor = "pointer";
